4dd08c867631e6414875cea78ac62f4fc2aaa7e6
zix <zabel11nakar11na@gmail.com>
2024-11-27 02:44:37 +0200

    little fixes

--- users/views.py
+++ users/views.py
@@ -12,34 +12,6 @@ from .services import create_product, create_price, create_checkout_session
 from .models import Payments
 
 
-@api_view(['POST'])
-def create_payment(request):
-    product_name = request.data.get('product_name')
-    amount = request.data.get('amount')  # Сумма в рублях
-
-    # Создание продукта и цены в Stripe
-    product = create_product(product_name)
-    price = create_price(product.id, amount * 100)  # Умножаем на 100 для копеек
-
-    # Создание сессии для оплаты
-    session = create_checkout_session(price.id)
-
-    # Сохранение информации о платеже в базе данных
-    payment = Payments.objects.create(
-        product_name=product_name,
-        amount=amount * 100,
-        stripe_session_id=session.id,
-    )
-
-    return Response({'payment_link': session.url})
-
-
-@api_view(['GET'])
-def get_payment_status(request, session_id):
-    session = stripe.checkout.Session.retrieve(session_id)
-    return Response({'status': session.payment_status})
-
-
 class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -74,3 +46,29 @@ class PaymentListAPIView(ListAPIView):
 class PaymentCreateAPIView(CreateAPIView):
     queryset = Payments.objects.all()
     serializer_class = PaymentSerializer
+
+    @api_view(['POST'])
+    def create_payment(request):
+        product_name = request.data.get('product_name')
+        amount = request.data.get('amount')  # Сумма в рублях
+
+        # Создание продукта и цены в Stripe
+        product = create_product(product_name)
+        price = create_price(product.id, amount * 100)  # Умножаем на 100 для копеек
+
+        # Создание сессии для оплаты
+        session = create_checkout_session(price.id)
+
+        # Сохранение информации о платеже в базе данных
+        payment = Payments.objects.create(
+            product_name=product_name,
+            amount=amount * 100,
+            stripe_session_id=session.id,
+        )
+
+        return Response({'payment_link': session.url})
+
+    @api_view(['GET'])
+    def get_payment_status(request, session_id):
+        session = stripe.checkout.Session.retrieve(session_id)
+        return Response({'status': session.payment_status})
